ea39723ed83ed59f1250e0558b29d4b23966be50
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-10-17 10:20:03 +0300

    update user data refacotred and make the abstract class file done

--- lib/Features/profile_feature.dart/data/user_profile_videos_remote_data_source/update_user_data_remote_data_source.dart
+++ lib/Features/profile_feature.dart/data/user_profile_videos_remote_data_source/update_user_data_remote_data_source.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shorts/Features/authentication_feature/data/user_model/user_model.dart';
 import 'package:shorts/Features/profile_feature.dart/domain/update_model/update_request_model.dart';
 import 'package:shorts/core/network/firebase_manager/collection_names.dart';
+import 'package:shorts/core/update_user_data/update_user_data.dart';
 import 'package:shorts/firebase_helper.dart';
 
 abstract class UpdateUserDataRemoteDataSource {
@@ -37,15 +38,13 @@ class UpdateUserDataSourceImpl implements UpdateUserDataRemoteDataSource {
       }
     }
 
-    // Update Firestore document
-    await _updateUserCollection(userId, updateUserRequestModel);
-
-    // Call functions to update all associated Firestore records
-    await _usersVideos(userId, updateUserRequestModel);
-    await _videosCollectionComments(userId, updateUserRequestModel);
-    await _videosCollection(userId, updateUserRequestModel);
-    await _usersFavourites(userId, updateUserRequestModel);
-    await _usersComments(userId, updateUserRequestModel);
+    await UpdateUserData.updateUserData(
+      updateUserRequestModel: updateUserRequestModel,
+      userId: userId,
+      firebaseHelper: firebaseHelper,
+      firestore: firestore,
+      auth: auth,
+    );
 
     DocumentSnapshot userDoc =
         await firestore.collection(CollectionNames.users).doc(userId).get();
@@ -60,130 +59,5 @@ class UpdateUserDataSourceImpl implements UpdateUserDataRemoteDataSource {
   }) async {
     await currentUser.verifyBeforeUpdateEmail(newEmail);
     await currentUser.reload();
-  }
-
-  Future<void> _updateUserCollection(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    await firestore.collection(CollectionNames.users).doc(userId).update(
-          updateUserRequestModel.toMap(),
-        );
-  }
-
-  Future<void> _usersComments(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    await firestore
-        .collection(CollectionNames.users)
-        .doc(userId)
-        .collection(CollectionNames.videos)
-        .get()
-        .then((videosQuerySnapshot) {
-      for (var videoDoc in videosQuerySnapshot.docs) {
-        videoDoc.reference
-            .collection(CollectionNames.comments)
-            .where('user.id', isEqualTo: userId)
-            .get()
-            .then((commentsQuerySnapshot) {
-          for (var commentDoc in commentsQuerySnapshot.docs) {
-            _userUpdatedData(commentDoc, updateUserRequestModel);
-          }
-        });
-      }
-    });
-  }
-
-  Future<void> _usersFavourites(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    // Fetch the user's favorite documents where the user's ID matches
-    List<Map<String, dynamic>> favourites =
-        await firebaseHelper.getCollectionDocuments(
-      collectionPath:
-          '${CollectionNames.users}/$userId/${CollectionNames.favourites}',
-      whereField: 'user.id',
-      whereValue: userId,
-    );
-
-    // Update each favorite document with the new user data
-    for (var favourite in favourites) {
-      await firebaseHelper.updateDocument(
-        collectionPath:
-            '${CollectionNames.users}/$userId/${CollectionNames.favourites}',
-        docId: favourite['id'], // Assuming each favorite has an 'id' field
-        data: {
-          'user.name': updateUserRequestModel.name,
-          'user.profilePic': updateUserRequestModel.imageUrl,
-          'user.email': updateUserRequestModel.email,
-          'user.phone': updateUserRequestModel.phone,
-        },
-      );
-    }
-  }
-
-  Future<void> _videosCollectionComments(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    await firestore
-        .collection(CollectionNames.videos)
-        .where('user.id', isEqualTo: userId)
-        .get()
-        .then((videoQuerySnapshot) {
-      for (var videoDoc in videoQuerySnapshot.docs) {
-        videoDoc.reference
-            .collection(CollectionNames.comments)
-            .where('user.id', isEqualTo: userId)
-            .get()
-            .then((commentsQuerySnapshot) {
-          for (var commentDoc in commentsQuerySnapshot.docs) {
-            _userUpdatedData(commentDoc, updateUserRequestModel);
-          }
-        });
-      }
-    });
-  }
-
-  Future<void> _videosCollection(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    List<Map<String, dynamic>> videos =
-        await firebaseHelper.getCollectionDocuments(
-      collectionPath: CollectionNames.videos,
-      whereField: 'user.id',
-      whereValue: userId,
-    );
-
-    for (var video in videos) {
-      await firebaseHelper.updateDocument(
-        collectionPath: CollectionNames.videos,
-        docId: video['id'],
-        data: {
-          'user.name': updateUserRequestModel.name,
-          'user.profilePic': updateUserRequestModel.imageUrl,
-          'user.email': updateUserRequestModel.email,
-          'user.phone': updateUserRequestModel.phone,
-        },
-      );
-    }
-  }
-
-  Future<void> _usersVideos(
-      String userId, UpdateUserRequestModel updateUserRequestModel) async {
-    await firestore
-        .collection(CollectionNames.users)
-        .doc(userId)
-        .collection(CollectionNames.videos)
-        .get()
-        .then((videoQuerySnapshot) {
-      for (var videoDoc in videoQuerySnapshot.docs) {
-        _userUpdatedData(videoDoc, updateUserRequestModel);
-      }
-    });
-  }
-
-  Future<void> _userUpdatedData(
-      QueryDocumentSnapshot<Map<String, dynamic>> videoDoc,
-      UpdateUserRequestModel updateUserRequestModel) {
-    return videoDoc.reference.update({
-      'user.name': updateUserRequestModel.name,
-      'user.profilePic': updateUserRequestModel.imageUrl,
-      'user.email': updateUserRequestModel.email,
-      'user.phone': updateUserRequestModel.phone,
-    });
   }
 }
---
+++ lib/core/update_user_data/update_user_data.dart
@@ -0,0 +1,203 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shorts/Features/profile_feature.dart/domain/update_model/update_request_model.dart';
+import 'package:shorts/core/network/firebase_manager/collection_names.dart';
+import 'package:shorts/firebase_helper.dart';
+
+abstract class UpdateUserData {
+  const UpdateUserData._();
+
+  static Future<void> updateUserData({
+    required UpdateUserRequestModel updateUserRequestModel,
+    required String userId,
+    required FirebaseHelperManager firebaseHelper,
+    required FirebaseFirestore firestore,
+    required FirebaseAuth auth,
+  }) async {
+    await _usersVideos(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firebaseHelper: firebaseHelper,
+    );
+    await _videosCollectionComments(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firestore: firestore,
+    );
+    await _videosCollection(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firebaseHelper: firebaseHelper,
+    );
+    await _usersFavourites(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firebaseHelper: firebaseHelper,
+    );
+    await _usersComments(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firestore: firestore,
+    );
+
+    await _updateUserCollection(
+      userId: userId,
+      updateUserRequestModel: updateUserRequestModel,
+      firebaseHelper: firebaseHelper,
+    );  
+  }
+
+
+ static Future<void> _updateUserCollection({
+   required String userId,
+    required UpdateUserRequestModel updateUserRequestModel,
+    required FirebaseHelperManager firebaseHelper,
+ }) async {
+    await firebaseHelper.updateDocument(
+  collectionPath: CollectionNames.users,
+  docId: userId,
+  data: updateUserRequestModel.toMap(),
+);
+  }
+
+  static Future<void> _usersComments({
+     required String userId,
+      required UpdateUserRequestModel updateUserRequestModel,
+      required FirebaseFirestore firestore,
+  }) async {
+    await firestore
+        .collection(CollectionNames.users)
+        .doc(userId)
+        .collection(CollectionNames.videos)
+        .get()
+        .then((videosQuerySnapshot) {
+      for (var videoDoc in videosQuerySnapshot.docs) {
+        videoDoc.reference
+            .collection(CollectionNames.comments)
+            .where('user.id', isEqualTo: userId)
+            .get()
+            .then((commentsQuerySnapshot) {
+          for (var commentDoc in commentsQuerySnapshot.docs) {
+            _userUpdatedData(commentDoc, updateUserRequestModel);
+          }
+        });
+      }
+    });
+  }
+
+  static Future<void> _usersFavourites({
+    required String userId,
+    required UpdateUserRequestModel updateUserRequestModel,
+    required FirebaseHelperManager firebaseHelper,
+  }) async {
+    // Fetch the user's favorite documents where the user's ID matches
+    List<Map<String, dynamic>> favourites =
+        await firebaseHelper.getCollectionDocuments(
+      collectionPath:
+          '${CollectionNames.users}/$userId/${CollectionNames.favourites}',
+      whereField: 'user.id',
+      whereValue: userId,
+    );
+
+    for (var favourite in favourites) {
+      await firebaseHelper.updateDocument(
+        collectionPath:
+            '${CollectionNames.users}/$userId/${CollectionNames.favourites}',
+        docId: favourite['id'],
+        data: {
+          'user.name': updateUserRequestModel.name,
+          'user.profilePic': updateUserRequestModel.imageUrl,
+          'user.email': updateUserRequestModel.email,
+          'user.phone': updateUserRequestModel.phone,
+        },
+      );
+    }
+  }
+
+  static Future<void> _videosCollectionComments({
+    required String userId,
+    required UpdateUserRequestModel updateUserRequestModel,
+    required FirebaseFirestore firestore,
+  }) async {
+    await firestore
+        .collection(CollectionNames.videos)
+        .where('user.id', isEqualTo: userId)
+        .get()
+        .then((videoQuerySnapshot) {
+      for (var videoDoc in videoQuerySnapshot.docs) {
+        videoDoc.reference
+            .collection(CollectionNames.comments)
+            .where('user.id', isEqualTo: userId)
+            .get()
+            .then((commentsQuerySnapshot) {
+          for (var commentDoc in commentsQuerySnapshot.docs) {
+            _userUpdatedData(commentDoc, updateUserRequestModel);
+          }
+        });
+      }
+    });
+  }
+
+  static Future<void> _videosCollection({
+    required String userId,
+    required UpdateUserRequestModel updateUserRequestModel,
+    required FirebaseHelperManager firebaseHelper,
+  }) async {
+    List<Map<String, dynamic>> videos =
+        await firebaseHelper.getCollectionDocuments(
+      collectionPath: CollectionNames.videos,
+      whereField: 'user.id',
+      whereValue: userId,
+    );
+
+    for (var video in videos) {
+      await firebaseHelper.updateDocument(
+        collectionPath: CollectionNames.videos,
+        docId: video['id'],
+        data: {
+          'user.name': updateUserRequestModel.name,
+          'user.profilePic': updateUserRequestModel.imageUrl,
+          'user.email': updateUserRequestModel.email,
+          'user.phone': updateUserRequestModel.phone,
+        },
+      );
+    }
+  }
+
+  static Future<void> _usersVideos({
+    required String userId,
+    required UpdateUserRequestModel updateUserRequestModel,
+    required FirebaseHelperManager firebaseHelper,
+  }) async {
+    List<Map<String, dynamic>> videos =
+        await firebaseHelper.getCollectionDocuments(
+      collectionPath:
+          '${CollectionNames.users}/$userId/${CollectionNames.videos}',
+    );
+
+    for (var video in videos) {
+      await firebaseHelper.updateDocument(
+        collectionPath:
+            '${CollectionNames.users}/$userId/${CollectionNames.videos}',
+        docId: video['id'],
+        data: {
+          'user.name': updateUserRequestModel.name,
+          'user.profilePic': updateUserRequestModel.imageUrl,
+          'user.email': updateUserRequestModel.email,
+          'user.phone': updateUserRequestModel.phone,
+        },
+      );
+    }
+  }
+
+  static Future<void> _userUpdatedData(
+      QueryDocumentSnapshot<Map<String, dynamic>> videoDoc,
+      UpdateUserRequestModel updateUserRequestModel) {
+    return videoDoc.reference.update({
+      'user.name': updateUserRequestModel.name,
+      'user.profilePic': updateUserRequestModel.imageUrl,
+      'user.email': updateUserRequestModel.email,
+      'user.phone': updateUserRequestModel.phone,
+    });
+  }
+}
