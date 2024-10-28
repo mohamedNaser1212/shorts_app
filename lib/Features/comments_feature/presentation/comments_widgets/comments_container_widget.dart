a99926ee5a8853d0519d9c5716626e9e80a15c5d
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-10-28 14:43:08 +0300

    adding show more package to comments

--- lib/Features/comments_feature/presentation/comments_widgets/comments_container_widget.dart
+++ lib/Features/comments_feature/presentation/comments_widgets/comments_container_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 import 'package:shorts/Features/comments_feature/domain/comments_entity/comments_entity.dart';
 import 'package:shorts/Features/comments_feature/presentation/comments_widgets/comments_bottom_sheet.dart';
 import 'package:shorts/Features/comments_feature/presentation/comments_widgets/delete_comment_icon_widget.dart';
+import 'package:shorts/core/managers/styles_manager/color_manager.dart';
 import 'package:shorts/core/user_info/cubit/user_info_cubit.dart';
 import 'package:shorts/core/widgets/custom_title.dart';
 
@@ -20,20 +22,19 @@ class CommentsContainerWidget extends StatelessWidget {
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: ColorController.whiteColor,
           borderRadius: BorderRadius.circular(12),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
+              color: ColorController.greyColor.withOpacity(0.2),
               blurRadius: 8,
               offset: const Offset(0, 4),
             ),
           ],
-          border: Border.all(color: Colors.grey.shade300),
+          border: Border.all(color: ColorController.greyColor.withOpacity(0.2)),
         ),
         child: Padding(
-          padding:
-              const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
           child: Row(
             children: [
               Column(
@@ -46,16 +47,24 @@ class CommentsContainerWidget extends StatelessWidget {
                   const SizedBox(height: 8),
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.6,
-                    child: CustomTitle(
-                      title: comment.content,
-                      style: TitleStyle.style14,
+                    child: ReadMoreText(
+                      comment.content,
+                      trimLines: 2,
+                      trimMode: TrimMode.Line,
+                      trimCollapsedText: 'Show more',
+                      trimExpandedText: 'Show less',
+                      style: const TextStyle(
+                        fontSize: 14,
+                      ),
+                      moreStyle:
+                          const TextStyle(color: ColorController.blueAccent),
+                      lessStyle: const TextStyle(color: ColorController.blueAccent),
                     ),
                   ),
                 ],
               ),
               const Spacer(),
-              if (comment.user.id ==
-                  UserInfoCubit.get(context).userEntity!.id)
+              if (comment.user.id == UserInfoCubit.get(context).userEntity!.id)
                 DeleteCommentIconWidget(comment: comment, state: state),
             ],
           ),
@@ -64,4 +73,3 @@ class CommentsContainerWidget extends StatelessWidget {
     );
   }
 }
-
