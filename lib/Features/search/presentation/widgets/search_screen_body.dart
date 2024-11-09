e45320f867ddee5012ea92081ff3216f21855965
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-11-09 15:33:49 +0200

    adding search list view

--- lib/Features/search/presentation/widgets/search_screen_body.dart
+++ lib/Features/search/presentation/widgets/search_screen_body.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shorts/Features/search/presentation/widgets/search_list_view.dart';
 import 'package:shorts/core/managers/styles_manager/color_manager.dart';
 import 'package:shorts/core/widgets/custom_back_icon_widget.dart';
-import 'package:shorts/core/widgets/custom_title.dart';
 
-import '../../../../core/functions/navigations_functions.dart';
-import '../../../../core/user_info/domain/user_entity/user_entity.dart';
 import '../../../../core/widgets/reusable_text_form_field.dart';
-import '../../../profile_feature.dart/presentation/screens/user_profile_screen.dart';
 import '../cubit/search_cubit.dart';
 
 class SearchScreenBody extends StatelessWidget {
@@ -66,51 +63,7 @@ class SearchScreenBody extends StatelessWidget {
                 ],
               ),
             ),
-            Expanded(
-              child: BlocBuilder<SearchCubit, SearchState>(
-                builder: (context, state) {
-                  if (state is SearchLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is SearchLoaded) {
-                    return Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: ListView.builder(
-                        itemCount: state.searchResults.length,
-                        itemBuilder: (context, index) {
-                          UserEntity user = state.searchResults[index];
-                          return InkWell(
-                            onTap: () {
-                              NavigationManager.navigateTo(
-                                context: context,
-                                screen: UserProfileScreen(
-                                  user: user,
-                                  // isShared: false,
-                                  // user: user,
-                                ),
-                              );
-                            },
-                            child: ListTile(
-                              contentPadding: const EdgeInsets.all(8.0),
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(user.profilePic),
-                              ),
-                              title: CustomTitle(
-                                style: TitleStyle.style16Bold,
-                                title: user.name,
-                                color: ColorController.whiteColor,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  } else if (state is SearchError) {
-                    return Center(child: Text(state.errorMessage));
-                  }
-                  return const Center(child: Text('No search results.'));
-                },
-              ),
-            ),
+            const SearchListView(),
           ],
         ),
       ),
