9cb272fa1ec5ce592a9ebc30f0df0422e17413ea
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-11-19 11:04:01 +0200

    some refactors

--- lib/Features/profile_feature.dart/presentation/screens/edit_profile_screen.dart
+++ lib/Features/profile_feature.dart/presentation/screens/edit_profile_screen.dart
@@ -4,16 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shorts/Features/profile_feature.dart/domain/use_case/update_user_data_use_case.dart';
 import 'package:shorts/Features/profile_feature.dart/presentation/cubit/update_user_cubit/update_user_data_cubit.dart';
-import 'package:shorts/Features/profile_feature.dart/presentation/widgets/edit_profile_screen_body.dart';
-import 'package:shorts/core/image_notifiere_controller/image_notifiere_controller.dart';
 import 'package:shorts/core/managers/styles_manager/color_manager.dart';
 import 'package:shorts/core/service_locator/service_locator.dart';
 import 'package:shorts/core/user_info/cubit/user_info_cubit.dart';
-import 'package:shorts/core/widgets/custom_progress_indicator.dart';
 
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../../../core/widgets/custom_title.dart';
-import '../widgets/update_profile_elevated_button.dart';
+import '../widgets/edit_profile_components_widgets.dart';
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
@@ -29,22 +26,12 @@ class EditProfileScreenState extends State<EditProfileScreen> {
   late String imageUrl = '';
   late File imageFile = File('');
   final formKey = GlobalKey<FormState>();
-  late final ImageNotifierController imageNotifierController;
-
-  @override
-  void initState() {
-    super.initState();
-    imageNotifierController = ImageNotifierController(
-      emailController: nameController,
-    );
-  }
 
   @override
   void dispose() {
     nameController.dispose();
     phoneController.dispose();
     bioController.dispose();
-    imageNotifierController.dispose();
     super.dispose();
   }
 
@@ -93,32 +80,10 @@ class EditProfileScreenState extends State<EditProfileScreen> {
             centerTitle: true,
             showLeadingIcon: true,
           ),
-          body: BlockInternactionLoadingWidget(
-            isLoading: isLoading || state is UpdateUserDataLoadingState,
-            child: CustomScrollView(
-              scrollBehavior: const MaterialScrollBehavior(),
-              slivers: [
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Form(
-                      key: formKey,
-                      child: EditProfileScreenBody(state: this),
-                    ),
-                  ),
-                ),
-                SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      UpdateProfileElevatedButton(editState: this),
-                      const SizedBox(height: 30.0),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          body: EditProfileComponentsWidgets(
+            isLoading: isLoading,
+            state: state,
+            editProfileScreenState: this,
           ),
         );
       },
---
+++ lib/Features/profile_feature.dart/presentation/widgets/edit_profile_components_widgets.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:shorts/Features/profile_feature.dart/presentation/widgets/update_profile_elevated_button.dart';
+
+import '../../../../core/widgets/custom_progress_indicator.dart';
+import '../cubit/update_user_cubit/update_user_data_cubit.dart';
+import '../screens/edit_profile_screen.dart';
+import 'edit_profile_screen_body.dart';
+
+class EditProfileComponentsWidgets extends StatelessWidget {
+  const EditProfileComponentsWidgets({
+    super.key,
+    required this.isLoading,
+    required this.state,
+    required this.editProfileScreenState,
+  });
+
+  final bool isLoading;
+  final UpdateUserDataState state;
+  final EditProfileScreenState editProfileScreenState;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlockInternactionLoadingWidget(
+      isLoading: isLoading || state is UpdateUserDataLoadingState,
+      child: CustomScrollView(
+        scrollBehavior: const MaterialScrollBehavior(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: editProfileScreenState.formKey,
+                child: EditProfileScreenBody(state: editProfileScreenState),
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                UpdateProfileElevatedButton(editState: editProfileScreenState),
+                const SizedBox(height: 30.0),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
