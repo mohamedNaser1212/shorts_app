210aacd0ee61e026edf24d27f155a6f52c6dd499
mohamedNaser1212 <nasergamal33e@gmail.com>
2024-11-24 13:06:59 +0200

    commented unUsed sign_out_elevated_button

--- lib/Features/profile_feature.dart/presentation/widgets/sign_out_elevated_button.dart
+++ lib/Features/profile_feature.dart/presentation/widgets/sign_out_elevated_button.dart
@@ -1,40 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shorts/Features/authentication_feature/presentation/cubit/sign_out_cubit/sign_out_cubit.dart';
-import 'package:shorts/Features/authentication_feature/presentation/screens/login_screen.dart';
-import 'package:shorts/Features/profile_feature.dart/presentation/screens/edit_profile_screen.dart';
-import 'package:shorts/core/functions/navigations_functions.dart';
-import 'package:shorts/core/widgets/custom_elevated_botton.dart';
-
-class SignOutElevatedButton extends StatelessWidget {
-  const SignOutElevatedButton({super.key, required this.editState});
-  final EditProfileScreenState editState;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<SignOutCubit, SignOutState>(
-      listener: (context, state) {
-        if (state is SignOutSuccessState) {
-          NavigationManager.navigateAndFinish(
-              context: context, screen: const LoginScreen());
-        } else if (state is SignOutErrorState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(state.error),
-            ),
-          );
-        } else if (state is SignOutLoadingState) {
-          NavigationManager.navigateAndFinish(
-              context: context, screen: const LoginScreen());
-        }
-      },
-      builder: (context, state) {
-        // bool isLoading = state is SignOutLoadingState;
-
-        return CustomElevatedButton.signOutElevatedButton(
-          context: context,
-        );
-      },
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:shorts/Features/authentication_feature/presentation/cubit/sign_out_cubit/sign_out_cubit.dart';
+// import 'package:shorts/Features/authentication_feature/presentation/screens/login_screen.dart';
+// import 'package:shorts/Features/profile_feature.dart/presentation/screens/edit_profile_screen.dart';
+// import 'package:shorts/core/functions/navigations_functions.dart';
+// import 'package:shorts/core/widgets/custom_elevated_botton.dart';
+//
+// class SignOutElevatedButton extends StatelessWidget {
+//   const SignOutElevatedButton({super.key, required this.editState});
+//   final EditProfileScreenState editState;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocConsumer<SignOutCubit, SignOutState>(
+//       listener: (context, state) {
+//         if (state is SignOutSuccessState) {
+//           NavigationManager.navigateAndFinish(
+//               context: context, screen: const LoginScreen());
+//         } else if (state is SignOutErrorState) {
+//           ScaffoldMessenger.of(context).showSnackBar(
+//             SnackBar(
+//               content: Text(state.error),
+//             ),
+//           );
+//         }
+//       },
+//       builder: (context, state) {
+//         // bool isLoading = state is SignOutLoadingState;
+//
+//         return CustomElevatedButton.signOutElevatedButton(
+//           context: context,
+//         );
+//       },
+//     );
+//   }
+// }
